3dbd8b6daa0600149f7604e88efe558ecf5517b1
dantedoyl <makstorzhkov@yandex.ru>
2020-05-20 23:22:57 +0300

    'dz'

---
+++ dz/build/precache-manifest.275262e3d97852b6aa004f560a984dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1027fe5171b0ba12b5351caf546e7d43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74566196581cd696dc9e",
+    "url": "/static/css/main.9c174f5e.chunk.css"
+  },
+  {
+    "revision": "261d0e4c040857fe8a02",
+    "url": "/static/js/2.e54b926b.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.e54b926b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74566196581cd696dc9e",
+    "url": "/static/js/main.0c01619b.chunk.js"
+  },
+  {
+    "revision": "a9e92ad10f72dcef6429",
+    "url": "/static/js/runtime-main.f50bb790.js"
+  }
+]);
