d83774d5e9331ce33a9eb3e43ed03150fd037818
dantedoyl <makstorzhkov@yandex.ru>
2020-07-12 16:17:03 +0300

    'apt'

---
+++ apteka/build/precache-manifest.7dc91ae178588d3a3c490f16b31e6d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5c45a06c4c64bdbb06acd5aeefdd71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e38dcf54f7d4f17b0d6f",
+    "url": "/static/css/main.a0c9f2d2.chunk.css"
+  },
+  {
+    "revision": "60c7831aa0e974951713",
+    "url": "/static/js/2.1391a343.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.1391a343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38dcf54f7d4f17b0d6f",
+    "url": "/static/js/main.e1e0f4e0.chunk.js"
+  },
+  {
+    "revision": "a9e92ad10f72dcef6429",
+    "url": "/static/js/runtime-main.f50bb790.js"
+  }
+]);
